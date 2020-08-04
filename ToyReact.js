de55b6b09cd23c44d6b3137cb3e7b05b62828e70
cell <1746806993@qq.com>
2020-08-04 23:45:35 +0800

    add helloworld

--- ToyReact.js
+++ ToyReact.js
@@ -1,5 +1,15 @@
 export let ToyReact = {
-  createElement() {
-    debugger
+  createElement(type, attributes, ...children) {
+    let element = document.createElement(type);
+    for (let name in attributes) {
+      element.setAttribute(name, attributes[name]);
+    }
+    for (let child of children) {
+      if (typeof child === "string") {
+        child = document.createTextNode(child);
+      }
+      element.appendChild(child);
+    }
+    return element;
   }
 }
--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-let a = <MyComponent name="a" />
+import {ToyReact} from './ToyReact'
+
+
+let a = <div id="iad" name="a">
+  <span>Hello</span>
+  <span>World</span>
+  <span>!</span>
+</div>
+document.body.appendChild(a)
